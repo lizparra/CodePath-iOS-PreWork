919e6c897542724ebd4e6f9eaee61a76586baffd
lizparra <44618559+lizparra@users.noreply.github.com>
2020-05-23 12:08:50 -0400

    Initial Commit

---
+++ tipCalculatorCodePath/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipCalculatorCodePath
+//
+//  Created by Liz Parra on 5/23/20.
+//  Copyright © 2020 Liz Parra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ tipCalculatorCodePathTests/tipCalculatorCodePathTests.swift
@@ -0,0 +1,34 @@
+//
+//  tipCalculatorCodePathTests.swift
+//  tipCalculatorCodePathTests
+//
+//  Created by Liz Parra on 5/23/20.
+//  Copyright © 2020 Liz Parra. All rights reserved.
+//
+
+import XCTest
+@testable import tipCalculatorCodePath
+
+class tipCalculatorCodePathTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
